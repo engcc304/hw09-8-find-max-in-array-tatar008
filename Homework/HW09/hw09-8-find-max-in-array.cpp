84b7135c14f78b29d0e0eb47fb7af44420629aeb
tatar008 <jirapat_ja66@live.rmutl.ac.th>
2023-09-20 21:50:50 +0700

    he09-8

--- Homework/HW09/hw09-8-find-max-in-array.cpp
+++ Homework/HW09/hw09-8-find-max-in-array.cpp
@@ -41,3 +41,39 @@
         Array: 6 7 11 13 15 49 27 18
         The large 3rd element = 18
 */
+#include<stdio.h>
+int main() {
+    int n ;
+    int arr[100] ;
+    int i = 0 ;
+    printf( "Enter new element of Array :\n" ) ;
+    scanf( "%d", &n ) ;
+
+    for( i = 0 ; i < n ; i++ ) {
+        printf( "Input :\n" ) ;
+        scanf( "%d", &arr[i] ) ;
+    }
+
+    printf( "Array: " ) ;
+    for( i = 0 ; i < n ; i++ ) {
+        printf( "%d ", arr[i] ) ;
+    }
+    for (int j = 0; j < n - 1; j++) {
+        for (int k = 0; k < n - j - 1; k++) {
+            if (arr[k] < arr[k + 1]) {
+                int temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+            }
+        }
+    }
+
+    if (n >= 3) {
+        printf("\nThe large 3rd element = %d\n", arr[2]);
+    } else {
+        printf("\nThere are not enough elements for the 3rd largest element.\n");
+    }
+
+    
+    return 0 ;
+}
